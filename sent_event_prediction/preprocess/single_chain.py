1a371329d6a4d713b638afa6637c6c5d9639cb91
waltbai <bailong18b@ict.ac.cn>
2021-04-02 03:00:24 +0800

    Ongoing: negative_sampling

--- sent_event_prediction/preprocess/single_chain.py
+++ sent_event_prediction/preprocess/single_chain.py
@@ -1,6 +1,8 @@
 """Generate single chain data."""
 import logging
 import os
+import random
+from copy import copy
 
 from tqdm import tqdm
 
@@ -12,8 +14,51 @@ from sent_event_prediction.utils.document import document_iterator
 logger = logging.getLogger(__name__)
 
 
-def negative_sampling():
-    """Replace mention in sentence and replace entity in argument."""
+def replace_mention(sentence, old_mention, new_mention):
+    """Replace old mention in sentence with new mention."""
+    return sentence.replace(old_mention["text"], new_mention["text"])
+
+
+def negative_sampling(positive_event,
+                      negative_pool,
+                      protagonist,
+                      non_protagonist_entities,
+                      num_events=4):
+    """Sampling negative events from negative pool.
+
+    Entities in negative events are replaced with protagonist
+    and random non-protagonist entities.
+    Entity mentions in sentences are replaced, too.
+
+    :param positive_event: positive event
+    :param negative_pool: negative event pool
+    :param protagonist: protagonist entity
+    :param non_protagonist_entities: non-protagonist entities
+    :param num_events: number of negative events
+    """
+    negative_events = []
+    for _ in range(num_events):
+        # Sample a negative event
+        negative_event = random.choice(negative_pool)
+        while negative_event["verb_lemma"] == positive_event["verb_lemma"]:
+            negative_event = random.choice(negative_pool)
+        negative_event = copy(negative_event)
+        # Assign entity mapping
+        negative_entities = negative_event.get_entities()
+        negative_protagonist = random.choice(negative_entities)
+        negative_non_protagonist = [e for e in negative_entities if e is not negative_protagonist]
+        replace_non_protagonist = []
+        for e in negative_non_protagonist:
+            if len(non_protagonist_entities) > 0:
+                replace_non_protagonist.append(random.choice(non_protagonist_entities))
+            else:
+                replace_non_protagonist.append(e)
+        # Replace mention in sentence
+        # Replace entity
+        negative_event.replace_entity(negative_protagonist, protagonist)
+        for neg, rep in zip(negative_non_protagonist, replace_non_protagonist):
+            negative_event.replace_entity(neg, rep)
+
 
 
 def generate_single_train(corp_dir,
@@ -58,6 +103,7 @@ def generate_single_train(corp_dir,
                     # Get non protagonist entities
                     non_protagonist_entities = doc.non_protagonist_entities(protagonist)
                     # Make sample
+
 
 
 
